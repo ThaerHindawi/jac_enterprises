afe87f1f06dbe2df293daaaeee555304ef4bbe22
Brett Heiney-Martin <110037799+btheiney@users.noreply.github.com>
2023-09-10 12:08:31 -0400

    Updated fetch function to grab 4 paintings at random and display them accordingly

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from "react";
-import ArtworkCard from "./ArtworkCard";
 
 interface Artwork {
+    id: number,
     title: string,
     artist: string,
     artist_lifespan: string,
@@ -13,26 +13,44 @@ interface Artwork {
 
 function Artwork(props: Artwork) {
 
-    const [images, setImages] = useState<Artwork | null>(null);
+    const [images, setImages] = useState<Artwork[]>([]);
 
     useEffect(() => {
-        fetchArtwork();
+        fetchArtwork();      
     }, []);
 
     async function fetchArtwork() {
         try {
-            const api = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/788905');
-            const data = await api.json();
-            const transformedData: Artwork = {
-                title: data.title,
-                artist: data.artistDisplayName,
-                artist_lifespan: data.artistBeginDate + ' - ' + data.artistEndDate,
-                department: data.department,
-                image_url: data.primaryImage,
-                image_date: data.objectDate,
-                met_url: data.objectURL,
-            };
-            setImages(transformedData);
+
+            let ids = [];
+            const api_ids = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&departmentId=11&q=Painting');
+            const data_ids = await api_ids.json();
+            
+            for (let i = 0; i < 4; i++) {
+                let random = Math.floor(Math.random() * (1000));
+                let api = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + data_ids.objectIDs[random]);
+                let data = await api.json();
+
+                while (data.primaryImage == "" || data.primaryImage == null) {
+                    random = Math.floor(Math.random() * (1000));
+                    api = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + data_ids.objectIDs[random]);
+                    data = await api.json();
+                }
+                
+                    const transformedData: Artwork = {
+                        id: random,
+                        title: data.title,
+                        artist: data.artistDisplayName,
+                        artist_lifespan: data.artistBeginDate + ' - ' + data.artistEndDate,
+                        department: data.department,
+                        image_url: data.primaryImage,
+                        image_date: data.objectDate,
+                        met_url: data.objectURL,
+                    };
+                    setImages((prev) => {
+                        return [...prev, transformedData];
+                    });  
+            }
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -40,15 +58,22 @@ function Artwork(props: Artwork) {
 
     return (
         <>
-            <h1 className="mb-4">Art Gallery</h1>
             {console.log(images)}
-            <div className="card">
-                <img className="card-img-top" height="500" src={images?.image_url} alt={images?.title}></img>
-                <div className="card-body">
-                    <h3>{images?.title}</h3>
-                    <p>{images?.artist + ' (' + images?.artist_lifespan + ')'}</p>
-                    <a href={images?.met_url} className="btn btn-secondary">Visit Museum of Art</a>
-                </div>
+            <h1 className="mb-4">Art Gallery</h1>
+            
+            <div className="row gap-2">
+                {images.map((artwork) => {
+                    return (
+                        <div key={images.indexOf(artwork)} className="card col-sm">
+                            <img className="card-img-top" height="500" src={artwork.image_url} alt={artwork.title}></img>
+                            <div className="card-body">
+                                <h3>{artwork.title}</h3>
+                                <p>{artwork.artist + ' (' + artwork.artist_lifespan + ')'}</p>
+                                <a target="_blank" href={artwork.met_url} className="btn btn-secondary">Visit Museum of Art</a>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </>
     );
